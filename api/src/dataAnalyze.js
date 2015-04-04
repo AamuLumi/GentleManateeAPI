1168b8439528c13c1369d8f4a6710329b80bceef
AamuLumi <flokauder@hotmail.com>
2015-04-04 15:02:43 +0200

    Add base DataAnalyze module

---
+++ api/src/dataAnalyze.js
@@ -0,0 +1,46 @@
+var Champion = require('../models/Champion');
+var ChampionItem = require('../models/ChampionItem');
+var ChampionLane = require('../models/ChampionLane');
+var ChampionPlayerRank = require('../models/ChampionPlayerRank');
+var ChampionRole = require('../models/ChampionRole');
+var ChampionSpell = require('../models/ChampionSpell');
+var Match = require('../models/Match');
+var Team = require('../models/Team');
+
+var dataAnalyze = function(data){
+    
+};
+
+module.exports = dataAnalyze;
+
+function getChampionForData(data){
+    
+}
+
+function getChampionItemForData(data){
+
+}
+
+function getChampionLaneForData(data){
+    
+}
+
+function getChampionPlayerRankForData(data){
+    
+}
+
+function getChampionRoleForData(data){
+    
+}
+
+function getChampionSpellForData(data){
+    
+}
+
+function getMatchForData(data){
+    
+}
+
+function getTeamForData(data){
+    
+}
